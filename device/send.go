3c17a3f7a436e058a10ca2b2f81bb01342889347
Martin Basovnik <martin.basovnik@wandera.com>
2020-08-25 13:47:56 +0200

    fixup! WND-29495 - POC of GSO impl for linux devices - accomodate N+1 segments (to be on the safe side and prevent 2x grow of a buffer)

--- device/send.go
+++ device/send.go
@@ -607,7 +607,7 @@ func (peer *Peer) RoutineSequentialSender() {
 
 			// send message and return buffer to pool
 
-			err := peer.Send(elem.packet)
+			err := peer.SendBuffer(elem.packet)
 			if len(elem.packet) != MessageKeepaliveSize {
 				peer.timersDataSent()
 			}
